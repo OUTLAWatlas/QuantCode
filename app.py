d36a8c92fb6971cea2b0539bb76f2dc8b7857043
Krishna Patil <krishna.patil241@vit.edu>
2025-10-06 04:46:27 +0530

    fix: Ensure database initialization occurs after app configuration

--- app.py
+++ app.py
@@ -32,11 +32,13 @@ app = Flask(__name__)
 # Enable CORS for all routes
 CORS(app)
 
+
 # --------------------
 # Database configuration
 # --------------------
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 
 
